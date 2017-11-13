03b65808d49cdeeb282eb571761c71644d254df5
Alex Berger <alex@more-onion.com>
2017-11-13 17:05:31 +0100

    NavBar component, bootstrap style

---
+++ components/NavBar.vue
@@ -0,0 +1,23 @@
+<template>
+  <b-navbar toggleable="sm" type="light" variant="light">
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse is-nav id="nav-collapse">
+        <b-navbar-nav>
+          <li class="nav-item"><nuxt-link class="nav-link" to="/" exact>Index</nuxt-link></li>
+          <li class="nav-item" v-if="isAuthenticated"><nuxt-link class="nav-link" to="/home">Home</nuxt-link></li>
+        </b-navbar-nav>
+        <b-navbar-nav class="ml-auto">
+          <li class="nav-item" v-if="!isAuthenticated"><nuxt-link class="nav-link" to="/auth/sign-in">Sign in</nuxt-link></li>
+          <li class="nav-item" v-else><nuxt-link class="nav-link" to="/auth/sign-off">Sign off</nuxt-link></li>
+        </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+import { mapGetters } from 'vuex'
+
+export default {
+  computed: mapGetters(['isAuthenticated'])
+}
+</script>
