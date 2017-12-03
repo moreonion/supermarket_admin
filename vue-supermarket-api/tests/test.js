a4a90e7d1c4f23cd89c01e5bc95f6de33ae053d9
Alex Berger <alex@more-onion.com>
2017-12-03 18:58:10 +0100

    vue-supermarket-api: simple test

--- vue-supermarket-api/tests/test.js
+++ vue-supermarket-api/tests/test.js
@@ -1,7 +1,16 @@
 import { assert } from 'chai'
+import VueSupermarketApi from '../src/'
 
-describe('Truth', function() {
-  it('should be true', function() {
-    assert(true === true)
+// a stub constructor function to get to the this.$supermarket functions
+const Vue = function() {}
+Vue.prototype.a = {}
+
+describe('VueSupermarketApi', function() {
+  describe('the plugin functionality', function() {
+    it('should install', function() {
+      VueSupermarketApi.install(Vue)
+      const vm = new Vue()
+      assert.isDefined(vm.$supermarket)
+    })
   })
 })
