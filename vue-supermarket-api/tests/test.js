e982d17f2bcc3967007001caf906cdced1a349ce
Alex Berger <alex@more-onion.com>
2017-12-06 15:49:40 +0100

    vue-supermarket-api: tests: fix eslint errors

--- vue-supermarket-api/tests/test.js
+++ vue-supermarket-api/tests/test.js
@@ -1,13 +1,15 @@
+/* global describe, it */
 import { assert } from 'chai'
+
 import VueSupermarketApi from '../src/'
 
 // a stub constructor function to get to the this.$supermarket functions
-const Vue = function() {}
+const Vue = function () {}
 Vue.prototype.a = {}
 
-describe('VueSupermarketApi', function() {
-  describe('the plugin functionality', function() {
-    it('should install', function() {
+describe('VueSupermarketApi', function () {
+  describe('the plugin functionality', function () {
+    it('should install', function () {
       VueSupermarketApi.install(Vue)
       const vm = new Vue()
       assert.isDefined(vm.$supermarket)
